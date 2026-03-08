6c8c6e7ae7aff9f412bfae95a88586e6369a709f
Arleepta Mohapatra <arleeptamohapatra@outlook.com>
2026-03-08 12:43:43 -0600

    fix DB env

--- server/db.js
+++ server/db.js
@@ -6,8 +6,7 @@ dotenv.config()
 const { Pool } = pg
 
 const connectionString =
-  process.env.DATABASE_URL ||
-  'postgresql://arleepta:Schwab123@10.2.1.3:5432/bts_pizza'
+  process.env.DATABASE_URL || 'postgresql://arleepta:Schwab123@10.2.1.3:5432/bts_pizza'
 
 export const pool = new Pool({
   connectionString,
